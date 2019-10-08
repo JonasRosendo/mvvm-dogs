bde73e446d0cf440ffbedeef7f374fa710cfbbea
JonasRosendo <jonasrosendo@live.com>
2019-10-08 19:14:30 -0300

    feature: Creating settings screen

---
+++ app/src/main/java/com/jonasrosendo/mvvmdogs/view/SettingsFragment.kt
@@ -0,0 +1,14 @@
+package com.jonasrosendo.mvvmdogs.view
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+import com.jonasrosendo.mvvmdogs.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+}
